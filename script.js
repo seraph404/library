1f7984f5a2645e72067a786986aa20e9be361cd0
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-25 12:40:13 -0400

    Create Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hi");
+function Book(title, author, pages, haveRead) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor.");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
+  this.info = function () {
+    const readStatus = this.haveRead ? "already read" : "not read yet";
+    return `${this.title} by ${this.author}, ${pages} pages, ${readStatus}`;
+  };
+}
