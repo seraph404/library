45db05b4ca6cea0541e30fc5905b00f219f45892
Seraphina <211722638+seraph404@users.noreply.github.com>
2025-05-25 12:44:45 -0400

    Create addBookToLibrary function

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function Book(title, author, pages, haveRead) {
+const myLibrary = [];
+
+function Book(id, title, author, pages, haveRead) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor.");
   }
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -10,4 +13,10 @@ function Book(title, author, pages, haveRead) {
     const readStatus = this.haveRead ? "already read" : "not read yet";
     return `${this.title} by ${this.author}, ${pages} pages, ${readStatus}`;
   };
+}
+
+function addBookToLibrary(title, author, pages, haveRead) {
+  const id = crypto.randomUUID();
+  const book = new Book(id, title, author, pages, haveRead);
+  myLibrary.push(book);
 }
